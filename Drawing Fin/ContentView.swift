cc2d15eb59276b2f3104b439f7f4d7dd4aaa1d5e
Nick <nick@zappycode.com>
2019-11-07 14:58:02 +0900

    Initial Commit

---
+++ Drawing Fin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing Fin
+//
+//  Created by ZappyCode on 11/7/19.
+//  Copyright © 2019 ZappyCode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
